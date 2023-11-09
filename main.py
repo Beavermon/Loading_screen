aec2cdeea30771726630b43cb8c6d30df7311da0
Beavermon <108250433+Beavermon@users.noreply.github.com>
2023-11-08 16:14:55 -0800

    Updated main.py
    Added in new code

--- main.py
+++ main.py
@@ -1,68 +1,133 @@
-# requires python and PIP (pip comes installed with recent versions of pygame)
+# requires the latest version of Python and PIP installed. 
 
 import pygame
 import sys
 from button import Button
+from pygame import mixer
 
+# list of things imported ^^
+
+mixer.init()
 pygame.init()
 
-SCREEN = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Jazzy-Hands")
-
-# sets the screen size and window name 
-
-python = pygame.image.load('image/note.png')
-SCREEN.blit(python, (600, 600))
-
 BG = pygame.image.load("image/Background.png")
-# loads background^
+
+
+# window configuration ^^
 
 def get_font(size):
     return pygame.font.Font("font/font.ttf", size)
 
 
+# game \/
 def play():
+    font = pygame.font.Font(pygame.font.get_default_font(), 100)
+    combo = 0
     while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+        pygame.mouse.get_pos()
+        clock = pygame.time.Clock()
 
-        SCREEN.fill("black")
+        screen = pygame.display.set_mode((800, 600))
 
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+        # first we will create the size of locked keys
 
-        PLAY_BACK = Button(image=None, pos=(640, 460),
-                           text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+        class Key:
+            def __init__(self, x, y, color1, color2, key):
+                self.x = x
+                self.y = y
+                self.color1 = color1
+                self.color2 = color2
+                self.key = key
+                self.rect = pygame.Rect(self.x, self.y, 40, 40)
+                self.handled = False
 
-        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
-        PLAY_BACK.update(SCREEN)
+        # list of keys (a,s,d,f) & location of the 4 locked squares
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
-                    main_menu()
+        keys = [
+            Key(100, 500, (255, 0, 0), (220, 0, 0), pygame.K_a),
+            Key(200, 500, (0, 255, 0), (0, 220, 0), pygame.K_s),
+            Key(300, 500, (0, 0, 255), (0, 0, 220), pygame.K_d),
+            Key(400, 500, (255, 255, 0), (220, 220, 0), pygame.K_f),
+        ]
+
+        # let's load the map into the game
+        def load(map):
+            rects = []
+            mixer.music.load(map + ".mp3")
+            mixer.music.play()
+            f = open(map + ".txt", 'r')
+            data = f.readlines()
+
+            for y in range(len(data)):
+                for x in range(len(data[y])):
+                    if data[y][x] == '0':
+                        rects.append(pygame.Rect(keys[x].rect.centerx - 25, y * -100, 50, 25))
+            return rects
+
+        map_rect = load("freedom dive")
+
+        while True:
+            screen.fill((0, 0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            # now we will loop through the keys and handle the events
+            k = pygame.key.get_pressed()
+            for key in keys:
+                if k[key.key]:
+                    pygame.draw.rect(screen, key.color1, key.rect)
+                    key.handled = False
+                if not k[key.key]:
+                    pygame.draw.rect(screen, key.color2, key.rect)
+                    key.handled = True
+                # now when we press our keys they will change color
+            for rect in map_rect:
+                pygame.draw.rect(screen, (200, 0, 0), rect)
+                rect.y += 4
+
+                # speed ^^
+
+                for key in keys:
+                    if key.rect.colliderect(rect) and not key.handled:
+                        map_rect.remove(rect)
+                        combo += 1
+                        key.handled = True
+                        break
+                if keys[0].rect.bottom < rect.y:
+                    map_rect.remove(rect)
+                    combo = 0
+                # basically, if the rectangle's bottom thingy y coordinate is smaller (<) than the other rectangles y, \
+                # then it will set combo counter to 0
+                text = font.render(str(combo), True, "blue")
+                screen.blit(text, (10, 10))
+
+            pygame.display.update()
+            clock.tick(60)
+        screen.fill("black")
 
         pygame.display.update()
 
 
+# options \/
 def options():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("white")
+        screen.fill("white")
 
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_TEXT = get_font(45).render("Your goal is to reach 20 on the combo counter!", True, "Black")
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
         OPTIONS_BACK = Button(image=None, pos=(640, 460),
-                              text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+                              text_input="GO BACK!", font=get_font(75), base_color="Black", hovering_color="Red")
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
+        OPTIONS_BACK.update(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -77,7 +142,7 @@ def options():
 
 def main_menu():
     while True:
-        SCREEN.blit(BG, (0, 0))
+        screen.blit(BG, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
@@ -91,11 +156,11 @@ def main_menu():
         QUIT_BUTTON = Button(image=pygame.image.load("image/Quit Rect.png"), pos=(940, 550),
                              text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
-        SCREEN.blit(MENU_TEXT, MENU_RECT)
+        screen.blit(MENU_TEXT, MENU_RECT)
 
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
-            button.update(SCREEN)
+            button.update(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
